a503b466c51ff9368ef6fd55285d13cb4adc29d0
Deon Lee <jamie2deon@aol.com>
2018-04-07 17:18:50 -0500

    Finished user-signup

--- main.py
+++ main.py
@@ -7,54 +7,48 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    username_error = ''
-    password_error = ''
-    email_error = ''
-    return render_template('index.html', username_error=username_error, password_error=password_error, email_error=email_error)
-
-@app.route('/welcome', methods=['POST'])
-def welcome():
-    #username = request.form['username']
-    return render_template('welcome.html')
+    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def display_form():
-    return render_template('index.html', username='', username_error='', password='', password_error='', email='', email_error='')
-
 def validate_signin():
-    username = render_template('index.html')
-    password = render_template('index.html')
-    email = render_template('index.html')
+    username = request.form["username"]
+    password = request.form['password']
+    password_verify= request.form['password_verify']
+    email = request.form['email']
+    username_error=""
+    password_error=""
+    email_error=""
 
     if username=="":
         username_error="Username cannot be blank."
     elif " " in username:
         username_error="Username cannot contain a space."
     elif len(username) <3 or len(username) >20:
-        username_error="Username must be 3-20 characters in length."
+        username_error="Username must be 3-20 characters long."
 
     if password=="":
         password_error="Password cannot be blank."
-        password=""
     elif " " in password:
         password_error="Password cannot contain a space."
-        password=""
     elif len(password) <3 or len(password) >20:
         password_error="Password must be 3-20 characters in length."
-        password=""
     elif password != password_verify:
         password_error="Password must match password verification."
 
-    if " " in email:
-        email_error="Email cannot contain a space."
-    elif len(email) <3 or len(email) >20:
-        email_error="Email must be 3-20 characters in length."
-    elif "@" not in email:
-        email_error="Email not valid (must contain @)."
-    elif "." not in email:
-        email_error="Email not valid (must contain . [dot])."
-
+    if email != "":
+        if " " in email:
+            email_error="Email cannot contain a space."
+        elif len(email) <3 or len(email) >20:
+            email_error="Email must be 3-20 characters in length."
+        elif "@" not in email:
+            email_error="Email not valid (must contain @)."
+        elif "." not in email:
+            email_error="Email not valid (must contain . [dot])."
+    
+    if not username_error and not password_error and not email_error:
+       return render_template('welcome.html', username=username)
+     
     else:
-        return "redirect('/welcome={username}'.format(username))"
+       return render_template('index.html', username_error=username_error, password_error=password_error, email_error=email_error )
 
 app.run()
